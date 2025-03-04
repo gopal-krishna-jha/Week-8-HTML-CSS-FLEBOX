9ea5ba1a226dfd2206bffa068eead61c332d0a35
Gopal Krishna Jha <141767531+gopal-krishna-jha@users.noreply.github.com>
2025-03-04 23:21:29 +0530

    [Gopal Krishna Jha] Add Welcome File of Week -08 HTML, CSS and Flexbox.
    [Gopal Krishna Jha] Add Welcome File of Week -08 HTML, CSS and Flexbox.

---
+++ WelcomeToWeek8HTMLCSS.java
@@ -0,0 +1,6 @@
+//Create a WelcomeToWeek08HTMLCSS to print the welcome statement
+public class WelcomeToWeek08HTMLCSS{
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to Week-08 HTML, CSS and Flexbox Intro !\"");
+    }
+}
